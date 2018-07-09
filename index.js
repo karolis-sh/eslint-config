1144180ae7053d7058ca3fb2f12dcd9e6d3f3418
Karolis Šarapnickis <karolis.sarapnickis@tesonet.com>
2018-07-09 22:22:03 +0300

    fix(eslint): complete prettier react integration

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 module.exports = {
-  extends: ["airbnb", "prettier"],
+  extends: ["airbnb", "prettier", "prettier/react"],
   plugins: ["prettier"],
   rules: {
     "import/prefer-default-export": [0],
     "prettier/prettier": [2],
-    "react/jsx-filename-extension": [1, { extensions: [".js"] }],
+    "react/jsx-filename-extension": [1, { extensions: [".js"] }]
   }
 };
