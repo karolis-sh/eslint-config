7f9f7d1445ad2db4b5cb28ccd0674883c6fc2b30
Karolis Šarapnickis <karolis.sarapnickis@tesonet.com>
2018-07-09 22:14:14 +0300

    feat(eslint): add import/prefer-default-export rule

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ module.exports = {
   extends: ["airbnb", "prettier"],
   plugins: ["prettier"],
   rules: {
+    "import/prefer-default-export": [0],
     "prettier/prettier": [2],
-    "react/jsx-filename-extension": [1, { extensions: [".js"] }]
+    "react/jsx-filename-extension": [1, { extensions: [".js"] }],
   }
 };
