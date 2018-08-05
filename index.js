b6d73010e0704fa968f46edf95f10780b5d8b05a
Karolis Šarapnickis <karolis.sarapnickis@tesonet.com>
2018-08-05 19:56:58 +0300

    feat: disable react/require-default-props rule

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ module.exports = {
   rules: {
     "import/prefer-default-export": [0],
     "prettier/prettier": [2],
-    "react/jsx-filename-extension": [1, { extensions: [".js"] }]
+    "react/jsx-filename-extension": [1, { extensions: [".js"] }],
+    "react/require-default-props": [0]
   }
 };
