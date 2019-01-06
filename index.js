3aef55c1fe680ada6e7ffeabc6b4983c91fa26c9
Karolis Šarapnickis <pastas.k@gmail.com>
2019-01-06 12:51:32 +0200

    feat(prettier): expose common prettier config

--- index.js
+++ index.js
@@ -1,11 +1,1 @@
-module.exports = {
-  extends: ['airbnb', 'prettier', 'prettier/react'],
-  plugins: ['prettier'],
-  rules: {
-    'import/prefer-default-export': [0],
-    'prettier/prettier': [2],
-    'react/jsx-filename-extension': [1, { extensions: ['.js'] }],
-    'react/require-default-props': [0],
-    quotes: ['error', 'single', { avoidEscape: true, allowTemplateLiterals: false }],
-  },
-};
+module.exports = require('./eslint');
